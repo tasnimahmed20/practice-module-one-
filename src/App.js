5c6107e2ebc54475b9e8f7fff48926bb65cacc5c
tasnimahmed20 <tasnima156@gmail.com>
2021-02-28 15:56:40 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const styles = {
+    backgroundColor:'white',
+    color:'blue'
+  }
+ const name="Tasnim Ahemd"
+ const info={
+   name:"Tasnim Ahmed",
+  passion: "coding"}
+  const product=[
+    {name:"photoshop", price:"$90.99"},
+  {name:"Illustrator", price:"$99.99"}]
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +28,45 @@ function App() {
         >
           Learn React
         </a>
+        <p>My passion is : {info.passion}</p>
+        <h1 style={styles}>MY name is : {name}</h1>
+        <Person info={info}></Person>
+        <Product product={product[0]}></Product>
+        <Product product={product[1]}></Product>
       </header>
     </div>
   );
+}
+function Product(props){
+  console.log(props)
+const {name,price}=props.product
+  const productStyle={
+    border:'2px solid red', 
+    borderRadius:'10px',
+    backgroundColor:'white',
+    height:'250px',
+    width:'400px',
+    color:'black'
+  }
+  return (
+    <div style={productStyle}>
+      <h3>Name: {name}</h3>
+      <p>Product Price: {price}</p>
+      <button>Buy Now</button>
+    </div>
+  )
+
+}
+function Person(props){
+  const {name,passion} = props.info;
+  return (
+    <div style={{
+      border:'2px solid red',
+       padding:'30px'}}>
+      <h1>Name: {name}</h1>
+      <h1>Passion: {passion}</h1>
+    </div>
+  )
 }
 
 export default App;
